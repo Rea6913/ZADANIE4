4c1646ac96e9b2eb0b1fa1558810e074a640379f
Rea6913 <47655248+Rea6913@users.noreply.github.com>
2019-02-20 20:26:30 +0300

    Update StringHelper.cs

--- StringHelper.cs
+++ StringHelper.cs
@@ -7,7 +7,10 @@ namespace PadawansTask4
         public static int GetVowelCount(string str)
         {
             // put your code here
-            //throw new NotImplementedException();
+            if (str == null)
+                throw new ArgumentNullException();
+            if (str == "" )
+                throw new ArgumentException();
             int kol = 0;
             for (int i = 0; i < str.Length; i++)
                 if (str[i] == 'a' || str[i] == 'e' || str[i] == 'o' || str[i] == 'i' || str[i] == 'u')
